c8e0f79799dde7ebb93b8cc3b5a3dffaf096476b
Ben <ben.c.hawker@gmail.com>
2015-07-27 17:13:11 +0100

    Added Direction & Compass

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,9 +1,16 @@
 class Ship
 
+	COMPASS = [:N, :NE, :E, :SE, :S, :SW, :W, :NW]
   attr_reader :length
 
-  def initialize length
+  def initialize (length, direction)
     @length = length
+    @direction = direction
+    raise "Use N, NE, E etc." unless COMPASS.include?(direction)
+  end
+
+  def set_direction
+  	@direction
   end
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,10 +2,24 @@ require 'ship'
 
 describe Ship do
 
-subject(:ship){Ship.new(2)}
+subject(:ship){Ship.new(2, :N)}
 
   it "should respond to length" do
-    expect(ship).to respond_to (:length)
+   expect(ship).to respond_to (:length)
   end
+
+  #perhaps not required
+  it "should respond to set direction" do
+  	expect(ship).to respond_to (:set_direction)
+  end	
+
+ 	it "can set direction of the ship" do
+ 		test_ship = Ship.new(2, :N)
+ 		expect(ship.set_direction).to eq :N
+ 	end
+
+ 	it "raises an error with non standard direction" do
+ 		expect{Ship.new(4, "d")}.to raise_error "Use N, NE, E etc." 
+ 	end
 
 end
