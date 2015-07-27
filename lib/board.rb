ca71c8809f4f09cd44689d3b96667f4c3469c67c
Ben <ben.c.hawker@gmail.com>
2015-07-27 15:54:24 +0100

    Places ship on board completed

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,25 @@
 class Board
 
-  def place ship
+	attr_accessor :size, :grid
 
+	def initialize (size)
+		@size = size
+		draw_board
+	end
+
+	def draw_board
+		@grid = Hash.new
+		('A'..'J').each do |letter|
+			(1..10).each do |i|
+					@grid["#{letter}#{i}"] = "*"
+          print @grid["#{letter}#{i}"]
+      end
+    end
+ 		@grid
+	end
+
+  def place (ship, co_ord)
+  	@grid[co_ord] = "X"
   end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,8 +2,27 @@ require 'board'
 
 describe Board do
 
-  it "responds to #place with 1 arg" do
-    expect(subject).to respond_to(:place).with(1).argument
+subject (:board) {Board.new(10)}
+let (:ship) {double(:ship)}
+
+  it "responds to #place with 2 args" do
+    expect(board).to respond_to(:place).with(2).argument
+  end
+
+  it "places a ship on the board" do
+  	subject.place (ship, "A1")
+  	expect(subject.grid["A1"]).to eq "X"
+
+  	# x = board.draw_board["A1"]
+  	# expect(x).to eq "X"
+  end
+
+  it "draws a board" do
+  	expect(board.draw_board).to include("J10")
+  end
+
+  it "has a size of 10 on creation" do
+  	expect(board).to respond_to :size
   end
 
 end
